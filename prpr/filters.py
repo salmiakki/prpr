c7e63a0a41de3de49886cb5fa86c9bf3b0f2faa2
Lesha Pak <kapahel@gmail.com>
2021-05-20 03:35:07 +0300

    Add filters

---
+++ prpr/filters.py
@@ -0,0 +1,70 @@
+from __future__ import annotations
+
+from enum import Enum, auto
+from typing import Iterable, Optional
+
+from loguru import logger
+
+from prpr.homework import Homework, Status
+
+OPEN_STATUSES = {Status.OPEN, Status.IN_REVIEW}
+CLOSED_STATUSES = {Status.RESOLVED, Status.CLOSED}
+
+
+class Mode(Enum):
+    DEFAULT = auto()
+    ALL = auto()
+    OPEN = auto()
+    CLOSED = auto()
+
+    def __str__(self):
+        return self.name.lower()
+
+    def __repr__(self):
+        return str(self)
+
+    @staticmethod
+    def from_string(mode: str) -> Mode:
+        try:
+            return Mode[mode.upper()]
+        except KeyError:
+            logger.error(f"Unexpected mode: '{mode}' 😿")
+            return mode
+
+
+def filter_homeworks(
+    homeworks: list[Homework],
+    *,
+    mode: Mode,
+    problems: Optional[list[int]] = None,
+    no: Optional[int] = None,
+    student: Optional[str] = None,
+) -> list[Homework]:
+    # TODO: return description as well to be used in the table title
+    if no:
+        result = [h for h in homeworks if h.number == no]
+        if not result:
+            logger.error(f"Homework with no {no} was not found 😿")
+            exit(1)
+        return result
+
+    if mode == Mode.DEFAULT:
+        result = _filter_homeworks_by_status(homeworks, CLOSED_STATUSES, invert=True)
+    elif mode == Mode.ALL:
+        result = homeworks
+    elif mode == Mode.OPEN:
+        result = _filter_homeworks_by_status(homeworks, OPEN_STATUSES)
+    elif mode == Mode.CLOSED:
+        result = _filter_homeworks_by_status(homeworks, CLOSED_STATUSES)
+
+    if problems:
+        result = [h for h in result if h.problem in problems]
+    if student:
+        result = [h for h in result if student in h.student]
+    return result
+
+
+def _filter_homeworks_by_status(homeworks: Iterable[Homework], statuses: set[Status], invert: bool = False):
+    if not invert:
+        return [h for h in homeworks if h.status in statuses]
+    return [h for h in homeworks if h.status not in statuses]
--- prpr/main.py
+++ prpr/main.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
+from __future__ import annotations
+
+import sys
 
 from loguru import logger
 
+from prpr.cli import configure_arg_parser
 from prpr.config import get_config
-from prpr.homework import Homework, Status
+from prpr.filters import filter_homeworks
+from prpr.homework import Homework
 from prpr.startrack_client import get_startack_client
 from prpr.table import DISPLAYED_TAIL_LENGTH, print_issue_table
 
@@ -12,16 +17,19 @@ def sort_homeworks(homeworks: list[Homework]) -> list[Homework]:
     return sorted(homeworks, key=Homework.order_key)
 
 
-def filter_homeworks(homeworks: list[Homework]) -> list[Homework]:
-    return [h for h in homeworks if h.status not in (Status.RESOLVED, Status.CLOSED)]
-
-
 def main():
+    arg_parser = configure_arg_parser()
+    args = arg_parser.parse_args()
+
+    configure_logger(args.verbose)
+    logger.debug(f"{args=}")
+
     config = get_config()
     client = get_startack_client(config)
 
     issues = client.get_issues()
     logger.debug(f"Got {len(issues)} homeworks.")
+
     homeworks = [
         Homework(
             issue_key=issue.key,
@@ -34,9 +42,21 @@ def main():
         )
         for number, issue in enumerate(issues, 1)
     ]
-    filtered_homeworks = filter_homeworks(homeworks)
+    filtered_homeworks = filter_homeworks(
+        homeworks,
+        mode=args.mode,
+        problems=args.problems,
+        no=args.no,
+        student=args.student,
+    )
     sorted_homeworks = sort_homeworks(filtered_homeworks)
     print_issue_table(sorted_homeworks, last=DISPLAYED_TAIL_LENGTH)
+
+
+def configure_logger(verbose):
+    if not verbose:
+        logger.remove()
+        logger.add(sys.stderr, level="WARNING")
 
 
 if __name__ == "__main__":
